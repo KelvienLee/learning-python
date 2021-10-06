9d5035560f7d0e88d9eb9337e4fb03f7edd80da7
KelvienLee <lixingqiang06@gmail.com>
2021-10-06 22:44:27 +0800

    开始编写单词爬取脚本

--- Words/main.py
+++ Words/main.py
@@ -1,7 +1,30 @@
 import requests
-
-url = 'https://github.com/jbyuki/instant.nvim'
-r_get = requests.get(url)
+import re
 
 
-print(r_get.text)
+search_word = 'test'
+URL = f'https://cn.bing.com/dict/search?q={search_word}'
+
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == '__main__':
+    html = get_html(URL)
+
+    word = re.findall('<h1.*?<strong>(.*?)</strong>', html, re.DOTALL)[0]
+    us_soundmark = re.findall('<div class="hd_prUS b_primtxt">.*?;(.*?)</div>', html, re.DOTALL)[0]
+    uk_soundmark = re.findall('<div class="hd_pr b_primtxt">.*?;(.*?)</div>', html, re.DOTALL)[0]
+    us_sounds = re.findall('<a class="bigaud".*?(https://.*?.mp3).*?</a>', html, re.DOTALL)[0]
+    uk_sounds = re.findall('<a class="bigaud".*?(https://.*?.mp3).*?</a>', html, re.DOTALL)[1]
+    in_list = re.findall('<div class="hd_area">.*?</div>.*?<li>(.*?)</li></ul>', html, re.DOTALL)
+
+    print(word)
+    print('美', us_soundmark)
+    print('英', uk_soundmark)
+    print(us_sounds)
+    print(uk_sounds)
+    print(len(in_list))
+    print(in_list)
